de0c079b9bb15b3dbb5e52c280c17f2cf5cae6db
Lee <lee.k.clarke@gmail.com>
2011-05-18 13:26:28 -0400

    fixed test by setting unique globals.

--- html5Game/js-test/SpriteTileManagerTest.js
+++ html5Game/js-test/SpriteTileManagerTest.js
@@ -1,10 +1,10 @@
 SpriteTileManagerTest = TestCase("SpriteTileManagerTest");
 
 //Setup test data.
-var tileMapManager = new SpriteTileManager(null, 32,32,"../res/dungeontiles.gif");
-var tile = {"id":0,"name":"FLOOR1","col":1,"row":2};
-var tile2 = {"id":1,"name":"FLOOR2","col":0,"row":2};
-var tile3 = {"id":3,"name":"DOOR2","col":1,"row":6};
+var sprite_tileMapManager = new SpriteTileManager(null, 32,32,"../res/dungeontiles.gif");
+var sprite_tile = {"id":0,"name":"FLOOR1","col":1,"row":2};
+var sprite_tile2 = {"id":1,"name":"FLOOR2","col":0,"row":2};
+var sprite_tile3 = {"id":3,"name":"DOOR2","col":1,"row":6};
 
 sprite_testManagerConfig = {"tileWidth":32, "tileHeight":32, "src":"res/dungeontiles.gif", "namedTiles":[
 		{"id":0,"name":"WALL1","col":0,"row":0},
@@ -16,21 +16,21 @@ sprite_testManagerConfig = {"tileWidth":32, "tileHeight":32, "src":"res/dungeont
 SpriteTileManagerTest.prototype.testGetNamedTileById = function() {
 	tileMapManagerObj = new SpriteTileManager(sprite_testManagerConfig);
 			
-	assertEquals(tile3,tileMapManagerObj.getNamedTileById(3));
+	assertEquals(sprite_tile3,tileMapManagerObj.getNamedTileById(3));
 }
 
 SpriteTileManagerTest.prototype.testAddNamedTile_object = function() {
-	tileMapManager.addNamedTile(tile);
-	tileMapManager.addNamedTile(tile2);
-	assertEquals(tile,tileMapManager.getNamedTileById(0));
+	sprite_tileMapManager.addNamedTile(sprite_tile);
+	sprite_tileMapManager.addNamedTile(sprite_tile2);
+	assertEquals(sprite_tile,sprite_tileMapManager.getNamedTileById(0));
 }
 
 SpriteTileManagerTest.prototype.testGetNamedTile = function() {
-	tileMapManager.addNamedTile(tile);
-	assertEquals(tile,tileMapManager.getNamedTile('FLOOR1'));
+	sprite_tileMapManager.addNamedTile(sprite_tile);
+	assertEquals(sprite_tile,sprite_tileMapManager.getNamedTile('FLOOR1'));
 }
 
 SpriteTileManagerTest.prototype.testGetNamedTileFail = function() {
-	tileMapManager.addNamedTile(tile);
-	assertNotSame(tile2,tileMapManager.getNamedTile('FLOOR1'));
+	sprite_tileMapManager.addNamedTile(sprite_tile);
+	assertNotSame(sprite_tile2,sprite_tileMapManager.getNamedTile('FLOOR1'));
 }
